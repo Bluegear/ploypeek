866b6d228173dbe88fb64c48872a45cbffb269e4
Chatri Sae-Tung <gearmag@gmail.com>
2017-09-25 14:40:19 +0700

    remove duplicate content

--- src/List.js
+++ src/List.js
@@ -5,7 +5,6 @@ class List extends Component {
     return (
       <div className="List">
         <ul>
-          <li><a href="/posts/what-is-ico">ICO คืออะไร?</a></li>
           <li><a href="/posts/firstpost">First post</a></li>
         </ul>
       </div>
